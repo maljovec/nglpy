75ca14f7be72b846d3f263d1f838ddb2a43ff0a7
Dan Maljovec <maljovec002@gmail.com>
2018-02-28 11:15:00 -0700

    Incrementing version number and conforming to PEP8 standards. Closes #4.

--- setup.py
+++ setup.py
@@ -1,40 +1,42 @@
- ##############################################################################
- # Software License Agreement (BSD License)                                   #
- #                                                                            #
- # Copyright 2018 University of Utah                                          #
- # Scientific Computing and Imaging Institute                                 #
- # 72 S Central Campus Drive, Room 3750                                       #
- # Salt Lake City, UT 84112                                                   #
- #                                                                            #
- # THE BSD LICENSE                                                            #
- #                                                                            #
- # Redistribution and use in source and binary forms, with or without         #
- # modification, are permitted provided that the following conditions         #
- # are met:                                                                   #
- #                                                                            #
- # 1. Redistributions of source code must retain the above copyright          #
- #    notice, this list of conditions and the following disclaimer.           #
- # 2. Redistributions in binary form must reproduce the above copyright       #
- #    notice, this list of conditions and the following disclaimer in the     #
- #    documentation and/or other materials provided with the distribution.    #
- # 3. Neither the name of the copyright holder nor the names of its           #
- #    contributors may be used to endorse or promote products derived         #
- #    from this software without specific prior written permission.           #
- #                                                                            #
- # THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR       #
- # IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  #
- # OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.    #
- # IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,           #
- # INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT   #
- # NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  #
- # DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY      #
- # THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT        #
- # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF   #
- # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.          #
- ##############################################################################
+########################################################################
+# Software License Agreement (BSD License)                             #
+#                                                                      #
+# Copyright 2018 University of Utah                                    #
+# Scientific Computing and Imaging Institute                           #
+# 72 S Central Campus Drive, Room 3750                                 #
+# Salt Lake City, UT 84112                                             #
+#                                                                      #
+# THE BSD LICENSE                                                      #
+#                                                                      #
+# Redistribution and use in source and binary forms, with or without   #
+# modification, are permitted provided that the following conditions   #
+# are met:                                                             #
+#                                                                      #
+# 1. Redistributions of source code must retain the above copyright    #
+#    notice, this list of conditions and the following disclaimer.     #
+# 2. Redistributions in binary form must reproduce the above copyright #
+#    notice, this list of conditions and the following disclaimer in   #
+#    the documentation and/or other materials provided with the        #
+#    distribution.                                                     #
+# 3. Neither the name of the copyright holder nor the names of its     #
+#    contributors may be used to endorse or promote products derived   #
+#    from this software without specific prior written permission.     #
+#                                                                      #
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR #
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED       #
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE   #
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY       #
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL   #
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE    #
+# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS        #
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER #
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR      #
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN  #
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                        #
+########################################################################
 """
-      Setup script for nglpy, a wrapper library for the C++ implementataion of
-      the neighborhood graph library (NGL).
+      Setup script for nglpy, a wrapper library for the C++
+      implementataion of the neighborhood graph library (NGL).
 """
 
 from setuptools import setup, Extension
@@ -54,8 +56,10 @@ def get_property(prop, project):
                        open(project + '/__init__.py').read())
     return result.group(1)
 
+
 FILES = ['ngl_wrap.cpp', 'GraphStructure.cpp', 'UnionFind.cpp']
 VERSION = get_property('__version__', 'nglpy')
+
 
 def long_description():
     """ Reads the README.md file and extracts the portion tagged between
@@ -73,25 +77,27 @@ def long_description():
 
             if recording:
                 description += line
-            
 
-## Consult here: https://packaging.python.org/tutorials/distributing-packages/
+
+# Consult here: https://packaging.python.org/tutorials/distributing-packages/
 setup(name='nglpy',
       packages=['nglpy'],
       version=VERSION,
-      description='A wrapper library for exposing the C++ neighborhood graph '
-                  + 'library (NGL) for computing empty region graphs to python',
+      description='A wrapper library for exposing the C++ neighborhood ' +
+                  'graph library (NGL) for computing empty region graphs to ' +
+                  'python',
       long_description=long_description(),
-      author = 'Dan Maljovec',
-      author_email = 'maljovec002@gmail.com',
-      license = 'BSD',
+      author='Dan Maljovec',
+      author_email='maljovec002@gmail.com',
+      license='BSD',
       test_suite='nglpy.tests',
-      url = 'https://github.com/maljovec/nglpy',
-      download_url = 'https://github.com/maljovec/nglpy/archive/'+VERSION+'.tar.gz',
-      keywords = ['geometry', 'neighborhood', 'empty region graph',
+      url='https://github.com/maljovec/nglpy',
+      download_url='https://github.com/maljovec/nglpy/archive/' + VERSION +
+                   '.tar.gz',
+      keywords=['geometry', 'neighborhood', 'empty region graph',
                   'neighborhood graph library', 'beta skeleton',
                   'relative neighbor', 'Gabriel graph'],
-      ## Consult here: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+      # Consult here: https://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
             'Development Status :: 3 - Alpha',
             'Intended Audience :: Science/Research',
