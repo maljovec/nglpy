3fff7507156f0bf87c3bd339fe7c53c4105725b7
Daniel Maljovec <daniel.maljovec@recursionpharma.com>
2019-11-19 22:37:00 -0700

    Trying to run make

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,7 @@
 #
 import os
 import sys
+import subprocess
 sys.path.insert(0, os.path.abspath('..'))
 
 
@@ -169,3 +170,5 @@ texinfo_documents = [
         "Miscellaneous",
     )
 ]
+
+subprocess.call(["cd .. && make"])
