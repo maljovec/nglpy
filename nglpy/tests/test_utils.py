827066573112ce6a7f856d1133af887a29ef255e
Daniel Maljovec <daniel.maljovec@recursionpharma.com>
2020-05-17 16:01:31 -0600

    Add boilerplate for utility testing

---
+++ nglpy/tests/test_utils.py
@@ -0,0 +1,18 @@
+""" This module will test the functionality of nglpy.utils
+"""
+import unittest
+import nglpy
+
+
+class TestUtils(unittest.TestCase):
+    """ Class for testing the utils
+    """
+
+    def test_consume_extra_args(self):
+        """ Tests the consume_extra_args function works.
+        """
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
