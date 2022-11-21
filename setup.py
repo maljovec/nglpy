a4c5fc0cbdb8c99578f9a3abfb6efc443399e4b6
dmaljovec <daniel.maljovec@recursionpharma.com>
2022-11-21 08:33:44 -0700

    fix: shift more to pyproject.toml, fix scikit-learn pin, and correct publishing of wheels

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
       implementataion of the neighborhood graph library (NGL).
 """
 
-import re
 import sys
 
 from setuptools import Extension, setup
@@ -16,24 +15,7 @@ if sys.platform == "darwin":
     extra_link_args = ["-stdlib=libc++"]
 
 
-def get_property(prop, project):
-    """
-    Helper function for retrieving properties from a project's
-    __init__.py file
-    @In, prop, string representing the property to be retrieved
-    @In, project, string representing the project from which we will
-    retrieve the property
-    @Out, string, the value of the found property
-    """
-    result = re.search(
-        r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop),
-        open(project + "/__init__.py").read(),
-    )
-    return result.group(1)
-
-
 FILES = ["ngl_wrap.cpp", "GraphStructure.cpp", "UnionFind.cpp"]
-VERSION = get_property("__version__", "nglpy")
 
 
 def long_description():
@@ -58,36 +40,11 @@ def long_description():
 setup(
     name="nglpy",
     packages=["nglpy"],
-    version=VERSION,
     description="A wrapper library for exposing the C++ neighborhood "
     + "graph library (NGL) for computing empty region graphs to "
     + "python",
     long_description=long_description(),
-    author="Dan Maljovec",
-    author_email="maljovec002@gmail.com",
-    license="BSD",
     test_suite="nglpy.tests",
-    url="https://github.com/maljovec/nglpy",
-    download_url="https://github.com/maljovec/nglpy/archive/" + VERSION + ".tar.gz",
-    keywords=[
-        "geometry",
-        "neighborhood",
-        "empty region graph",
-        "neighborhood graph library",
-        "beta skeleton",
-        "relative neighbor",
-        "Gabriel graph",
-    ],
-    # Consult here: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: C++",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering :: Mathematics",
-    ],
     install_requires=requirements,
     python_requires=">=2.7, <4",
     ext_modules=[
